ccbbe2346624d5e77ca7dc44f7d6a87393e15bc8
Meep220 <143765941+Meep220@users.noreply.github.com>
2026-03-03 12:22:08 +0800

    Add Java Code for Switching between Activities

--- app/src/main/java/com/example/studybuddy/Activity5.java
+++ app/src/main/java/com/example/studybuddy/Activity5.java
@@ -1,6 +1,9 @@
 package com.example.studybuddy;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,16 @@ public class Activity5 extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        Button NextBtn = findViewById(R.id.NextBtn);
+        NextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //1. Create the Intent (From Login (Or Main Activity here) to next Activity (To Activity 2))
+                Intent intent = new Intent(Activity5.this, MainActivity.class);
+                //2. Then Start the Activity
+                startActivity(intent);
+            }
         });
     }
 }
--- app/src/main/java/com/example/studybuddy/MainActivity.java
+++ app/src/main/java/com/example/studybuddy/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.studybuddy;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,16 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        Button loginBtn = findViewById(R.id.login_button);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //1. Create the Intent (From Login (Or Main Activity here) to next Activity (To Activity 2))
+                Intent intent = new Intent(MainActivity.this, Activity2.class);
+                //2. Then Start the Activity
+                startActivity(intent);
+            }
         });
     }
 }
